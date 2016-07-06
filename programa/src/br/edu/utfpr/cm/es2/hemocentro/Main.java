a5af9713bcb9a6b2de8d2afbb4667c6abbaab1d0
LuanBodner <luan.rosario.bodner@gmail.com>
2016-07-06 14:41:13 -0300

    Adicionando Controller

--- programa/src/br/edu/utfpr/cm/es2/hemocentro/Main.java
+++ programa/src/br/edu/utfpr/cm/es2/hemocentro/Main.java
@@ -3,6 +3,8 @@
  */
 package br.edu.utfpr.cm.es2.hemocentro;
 
+import interfaceGrafica.GraphicInterface;
+
 /**
  * @author felipe
  *
@@ -13,7 +15,9 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-
+    	
+    	GraphicInterface gi = new GraphicInterface();
+    	gi.setVisible(true);
     }
 
 }
